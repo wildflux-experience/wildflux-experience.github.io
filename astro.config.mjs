4aecf842e0aa9b268897f033771d067671e332f9
kernythree <mohanjulien@gmail.com>
2026-05-06 11:11:07 +0400

    change site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hkbertoson.github.io',
-    base: '/github-pages'
+    site: 'https://wildflux-experience.github.io',
+    base: ''
 });
